79d3e7a1a519ab83bbcdab054ec5daac4f5b25c6
scheyo <scheyo@naver.com>
2022-07-26 20:46:57 +0900

    test

---
+++ BJ/src/Gold/goldTest.java
@@ -0,0 +1,10 @@
+package Gold;
+
+public class goldTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Study/src/Day1_0713/test.java
@@ -0,0 +1,18 @@
+package Day1_0713;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class test {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		// 하이
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = 0;
+		
+	}
+
+}
